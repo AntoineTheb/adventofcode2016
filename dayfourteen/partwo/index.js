7466d50356d96cce75f4c3deb4d680fb1c720077
AntoineTheb <theberge.antoine.cem@gmail.com>
2016-12-15 10:58:52 -0500

    fourteen part 1

--- dayfourteen/partwo/index.js
+++ dayfourteen/partwo/index.js
@@ -1,31 +1,34 @@
 'use strict';
 const md5 = require('md5');
-const input = `zpqevtbw`;
+const input = `abc`;
 
 // hurray for the most inefficient answer ever !
 function keys() {
-  let i = 0;
+  let i;
   let max = 1000;
+  const memo = {};
   const potentials = [];
   const hashes = [];
-  while (hashes.length < 64) {
-    const hash = md5(`${input}${i}`);
+  for (i=0;hashes.length < 64; ++i) {
+    let hash = `${input}${i}`;
+    for (let j = 0; j <= 2016; ++j)
+      memo[hash] = hash = memo[hash] || md5(hash);
     const tmatches = hash.match(/(.)\1{2}/);
     if (tmatches) {
+      // console.log(hash, 'is a candidate');
       const tmatch = tmatches[0];
       for (let j = i + 1; j <= i + 1000; ++j) {
         const fhash = md5(`${input}${j}`);
-        const fmatches = fhash.match(/(.)\1{4}/);
+        const fmatches = fhash.match(`${tmatch[0]}{5}`, 'g');
         if (fmatches) {
           const fmatch = fmatches[0].slice(0, 3);
           if (tmatch === fmatch) {
-            console.log(tmatch, fmatches[0], i);
+            console.log(hash, fhash, i, j);
             hashes.push(hash);
           }
         }
       }
     }
-    i++;
   }
   console.log(hashes);
   return i - 1;
